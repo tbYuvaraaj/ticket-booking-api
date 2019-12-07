f01fb68da2daf0450ed0f3f2367213ce2dc3c1d8
Yuvaraj <tbyuvaraaj@gmail.com>
2019-12-07 09:34:20 +0530

    Initial commit

---
+++ src/test/java/com/project/book/ticket/ticketbookingapi/TicketBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.book.ticket.ticketbookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
