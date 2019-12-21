e87dae5e5f090971b157aaf821531311e06d2985
Yuvaraj <tbyuvaraaj@gmail.com>
2019-12-21 22:51:29 +0530

    Added customException handler and controllers

--- src/main/java/com/project/book/ticket/ticketbookingapi/controller/HelloWorldController.java
+++ src/main/java/com/project/book/ticket/ticketbookingapi/controller/HelloWorldController.java
@@ -1,13 +1,34 @@
 package com.project.book.ticket.ticketbookingapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.book.ticket.ticketbookingapi.exception.TicketNotFoundException;
+import com.project.book.ticket.ticketbookingapi.model.TicketDetails;
+import com.project.book.ticket.ticketbookingapi.service.TicketBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private  TicketBookingService ticketBookingService;
 
     @RequestMapping("/test")
     public String sample(){
         return "Good Morning..";
     }
+
+    @PostMapping("/reservation")
+    public void ticketReservation(@RequestBody TicketDetails details){
+        ticketBookingService.bookTicket(details);
+        System.out.println("Ticket has been booked!");
+    }
+
+    @GetMapping("/pnr/{no}")
+    public ResponseEntity<TicketDetails> getTicketDetails(@PathVariable("no") String pnrNo){
+        return  new ResponseEntity<TicketDetails>(ticketBookingService.getTicketDetails(pnrNo).orElseThrow(()->new TicketNotFoundException("pnrNo")), HttpStatus.OK);
+  }
 }
